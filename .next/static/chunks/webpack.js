125566fbd97524c56d6c35618c4d872dcca83302
Wender Barbosa <wenblack012@gmail.com>
2023-04-06 15:31:01 -0300

    ❌ remove .next folder from git ❌

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fda4d2e0bb0341da"; }
+/******/ 		__webpack_require__.h = function() { return "f4b7e87e49ff2392"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
